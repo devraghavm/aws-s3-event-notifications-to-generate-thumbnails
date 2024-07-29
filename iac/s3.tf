5439b2c1289441595cd495ee0fc383a5582ada8e
devraghavm <medapati.raghu@gmail.com>
2024-07-28 23:28:34 -0600

    chore: Update S3 bucket notification configuration

--- iac/s3.tf
+++ iac/s3.tf
@@ -12,16 +12,9 @@ resource "aws_s3_object" "thumbnails_folder" {
   key    = "thumbnails/"
 }
 
-resource "null_resource" "wait_for_lambda_trigger" {
-  depends_on = [aws_lambda_permission.apigw_lambda]
-  provisioner "local-exec" {
-    command = "sleep 3m"
-  }
-}
-
 resource "aws_s3_bucket_notification" "images_put_notification" {
-  bucket     = aws_s3_bucket.my-app-images.id
-  depends_on = [null_resource.wait_for_lambda_trigger]
+  bucket = aws_s3_bucket.my-app-images.id
+
   topic {
     topic_arn     = aws_sns_topic.topic.arn
     filter_prefix = "images/"
